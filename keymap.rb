1f5d7f6e8b850aba87c2d1c31bba093fabf07b87
vushthestampede <57708412+vushthestampede@users.noreply.github.com>
2023-05-10 00:42:21 +0200

    Update keymap.rb

--- keymap.rb
+++ keymap.rb
@@ -8,32 +8,56 @@ kbd.init_pins(
 )
 
 # default layer should be added at first
-kbd.add_layer :default, %i[
-  KC_ESC    KC_Q     KC_W      KC_E     KC_R     KC_T    KC_Y       KC_U       KC_I      KC_O      KC_P      KC_NO     KC_BSPC
-  KC_NO     KC_TAB   KC_A      KC_S     KC_D     KC_F    KC_G       KC_H       KC_J      KC_K      KC_L      KC_SCLN   KC_ENT
-  KC_NO     KC_NO    KC_LSFT   KC_Z     KC_X     KC_C    KC_V       KC_B       KC_N      KC_M      KC_COMM   KC_DOT    KC_UP     KC_RSFT
-  KC_NO     KC_NO    KC_LCTL   KC_LGUI  KC_LALT  KC_NO   SPC_RAISE  SPC_LOWER  KC_NO     KC_NO     KC_DEL    KC_LEFT   KC_DOWN   KC_RGHT
+
+#,-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.
+#|  Esc  |   a   |   z   |   e   |   r   |   t   |   y   |   u   |   i   |   o   |   p   |     Bspc      |
+#|-------'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-------|
+#|  Tab    |   q   |   s   |   d   |   f   |   g   |   h   |   j   |   k   |   l   |   m   |    Enter    |
+#|---------'--,----'--,----'--,----'--,----'--,----'--,----'--,----'--,----'--,----'--,----'--,----------|
+#|  Shift     |   w   |   x   |   c   |   v   |   b   |   n   |   ,   |   ;   |   :   |   up  |   Shift  |
+#|---------,--'------,'-------',------'-------'-------'-------'-------',------'--,----'--,----'--,-------|
+#|   Ctrl  |   Gui   |   Alt   |   Spc/Lower          |   Spc/Raise    |   Del   | left  |  down | right |
+#`---------'---------'---------'----------------------'----------------'---------'-------'-------'-------'
+
+kbd.add_layer :base, %i[
+  KC_ESC   KC_Q    KC_W     KC_E      KC_R     KC_T   KC_Y        KC_U       KC_I     KC_O     KC_P     KC_NO    KC_BSPC
+  KC_NO    KC_TAB  KC_A     KC_S      KC_D     KC_F   KC_G        KC_H       KC_J     KC_K     KC_L     KC_SCLN  KC_ENT
+  KC_NO    KC_NO   KC_LSFT  KC_Z      KC_X     KC_C   KC_V        KC_B       KC_N     KC_M     KC_COMM  KC_DOT   KC_UP    KC_RSFT
+  KC_NO    KC_NO   KC_LCTL  KC_LGUI   KC_LALT  KC_NO  SPC_LOWER   SPC_RAISE  KC_NO    KC_NO    KC_DEL   KC_LEFT  KC_DOWN  KC_RGHT
 ]
 
-kbd.add_layer :raise, %i[
-  KC_F1     KC_F2    KC_F3     KC_F4    KC_F5    KC_F6   KC_F7      KC_F8      KC_F9     KC_F10    KC_F11    KC_NO     KC_F12
-  KC_NO     KC_NO    KC_NO     KC_NO    KC_NO    KC_NO   KC_NO      KC_NO      KC_NO     KC_NO     KC_NO     DM_PLY1   NK_TOGG
-  KC_NO     KC_NO    KC_CAPS   KC_PAUS  KC_NO    KC_NO   KC_NO      KC_NO      KC_NO     KC_NO     KC_NO     KC_NO    DM_RSTP    DM_REC1
-  KC_NO     KC_NO    KC_TRNS   KC_TRNS  KC_TRNS  KC_NO   KC_TRNS    KC_PSCR    KC_NO     KC_NO     KC_RCTL   KC_NO    KC_NO      TO(3)
-]
+#,-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.
+#|   F1  |   F2  |   F3  |   F4  |   F5  |   F6  |   F7  |   F8  |   F9  |  F10  |  F11  |      F12      |
+#|-------'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-------|
+#|         |       |       |       |       |       |       |       |       |       |       |             |
+#|---------'--,----'--,----'--,----'--,----'--,----'--,----'--,----'--,----'--,----'--,----'--,----------|
+#| Caps Lock  | Pause |       |       |       |       |       |       |       |       | Vol + |   Shift  |
+#|---------,--'------,'-------',------'-------'-------'-------'-------',------'--,----'--,----'--,-------|
+#|   Ctrl  |   Gui   |   Alt   |   Spc/Lower          |  Print Screen  |  RCtrl  |       | Vol - |       |
+#`---------'---------'---------'----------------------'----------------'---------'-------'-------'-------'
 
 kbd.add_layer :lower, %i[
-  KC_1      KC_2     KC_3      KC_4     KC_5     KC_6    KC_7       KC_8       KC_9      KC_0      KC_MINS   KC_NO     KC_EQL
-  KC_NO     KC_GRV   KC_NO     KC_NO    KC_NO    KC_NO   KC_NO      KC_NO      KC_LBRC   KC_RBRC   KC_SCLN   KC_NUHS   KC_NO
-  KC_NO     KC_NO    KC_TRNS   KC_NUBS  KC_NO    KC_NO   KC_NO      KC_NO      KC_M      KC_COMM   KC_DOT    KC_SLSH   KC_VOLU   KC_TRNS
-  KC_NO     KC_NO    KC_TRNS   KC_TRNS  KC_TRNS  KC_NO   KC_MUTE    KC_TRNS    KC_NO     KC_NO     KC_RALT   KC_MPRV   KC_VOLD   KC_MNXT
+  KC_F1    KC_F2   KC_F3    KC_F4     KC_F5    KC_F6  KC_F7       KC_F8      KC_F9    KC_F10   KC_F11   KC_NO    KC_F12
+  KC_NO    KC_NO   KC_NO    KC_NO     KC_NO    KC_NO  KC_NO       KC_NO      KC_NO    KC_NO    KC_NO    KC_NO    KC_NO
+  KC_NO    KC_NO   KC_CAPS  KC_PAUSE  KC_NO    KC_NO  KC_NO       KC_NO      KC_NO    KC_NO    KC_NO    KC_NO    KC_KB_VOLUME_UP    KC_RSFT
+  KC_NO    KC_NO   KC_LCTL  KC_LGUI   KC_LALT  KC_NO  KC_TRNS     KC_PSCR    KC_NO    KC_NO    KC_RCTL  KC_NO    KC_KB_VOLUME_DOWN  KC_NO
 ]
 
-kbd.add_layer :adjust, %i[
-  TO(0)     KC_NO    KC_NO     KC_NO    KC_NO    KC_NO   KC_NO      KC_NO      KC_NO     KC_NO     KC_NO     KC_NO     KC_NO
-  KC_NO     KC_NO    KC_NO     KC_NO    KC_NO    KC_NO   KC_NO      KC_NO      KC_NO     KC_NO     KC_NO     KC_NO     KC_NO
-  KC_NO     KC_NO    KC_NO     KC_NO    KC_NO    KC_NO   KC_NO      KC_NO      KC_NO     KC_NO     KC_NO     KC_NO     KC_NO     KC_NO
-  KC_NO     KC_NO    KC_NO     KC_NO    KC_NO    KC_NO   KC_NO      KC_NO      KC_NO     KC_NO     KC_NO     KC_NO     KC_NO     KC_NO
+#,-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.
+#|   &   |   é   |   "   |   '   |   (   |   -   |   è   |   _   |   ç   |   à   |   )   |       =       |
+#|-------'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-------|
+#|         |   ²   |       |       |       |       |       |   ^   |   $   |   ù   |   *   |             |
+#|---------'--,----'--,----'--,----'--,----'--,----'--,----'--,----'--,----'--,----'--,----'--,----------|
+#|   Shift    |   <   |       |       |       |       |   ,   |   ;   |   :   |   !   | Vol + |   Shift  |
+#|---------,--'------,'-------',------'-------'-------'-------'-------',------'--,----'--,----'--,-------|
+#|  Ctrl   |   Gui   |   Alt   |   Mute               |   Spc/Raise    |  AltGr  |       | Vol - |       |
+#`---------'---------'---------'----------------------'----------------'---------'-------'-------'-------'
+
+kbd.add_layer :raise, %i[
+  KC_1     KC_2    KC_3      KC_4                KC_5     KC_6   KC_7        KC_8       KC_9     KC_0     KC_MINS       KC_NO          KC_EQL
+  KC_NO    KC_NO   KC_GRAVE  KC_NO               KC_NO    KC_NO  KC_NO       KC_NO      KC_LBRC  KC_RBRC  KC_SEMICOLON  KC_NONUS_HASH  KC_TRNS
+  KC_NO    KC_NO   KC_LSFT   KC_NONUS_BACKSLASH  KC_NO    KC_NO  KC_NO       KC_NO      KC_M     KC_COMM  KC_DOT        KC_SLSH        KC_KB_VOLUME_UP    KC_RSFT
+  KC_NO    KC_NO   KC_LCTL   KC_LGUI             KC_LALT  KC_NO  KC_KB_MUTE  KC_TRNS    KC_NO    KC_NO    KC_RALT       KC_NO          KC_KB_VOLUME_DOWN  KC_NO
 ]
 
 #
