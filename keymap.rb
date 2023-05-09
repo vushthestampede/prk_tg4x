a64b3aa4c0e1b45486eac327cc77e49865a587b8
vushthestampede <57708412+vushthestampede@users.noreply.github.com>
2023-05-10 01:00:47 +0200

    Update keymap.rb

--- keymap.rb
+++ keymap.rb
@@ -20,10 +20,10 @@ kbd.init_pins(
 #`---------'---------'---------'----------------------'----------------'---------'-------'-------'-------'
 
 kbd.add_layer :base, %i[
-  KC_ESC   KC_Q    KC_W     KC_E      KC_R     KC_T   KC_Y        KC_U       KC_I     KC_O     KC_P     KC_NO    KC_BSPC
-  KC_NO    KC_TAB  KC_A     KC_S      KC_D     KC_F   KC_G        KC_H       KC_J     KC_K     KC_L     KC_SCLN  KC_ENT
-  KC_NO    KC_NO   KC_LSFT  KC_Z      KC_X     KC_C   KC_V        KC_B       KC_N     KC_M     KC_COMM  KC_DOT   KC_UP    KC_RSFT
-  KC_NO    KC_NO   KC_LCTL  KC_LGUI   KC_LALT  KC_NO  SPC_LOWER   SPC_RAISE  KC_NO    KC_NO    KC_DEL   KC_LEFT  KC_DOWN  KC_RGHT
+  KC_ESC   KC_Q    KC_W     KC_E      KC_R     KC_T   KC_Y       KC_U       KC_I     KC_O     KC_P     KC_NO    KC_BSPC
+  KC_NO    KC_TAB  KC_A     KC_S      KC_D     KC_F   KC_G       KC_H       KC_J     KC_K     KC_L     KC_SCLN  KC_ENT
+  KC_NO    KC_NO   KC_LSFT  KC_Z      KC_X     KC_C   KC_V       KC_B       KC_N     KC_M     KC_COMM  KC_DOT   KC_UP    KC_RSFT
+  KC_NO    KC_NO   KC_LCTL  KC_LGUI   KC_LALT  KC_NO  SPC_LOWER  SPC_RAISE  KC_NO    KC_NO    KC_DEL   KC_LEFT  KC_DOWN  KC_RGHT
 ]
 
 #,-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.
@@ -37,10 +37,10 @@ kbd.add_layer :base, %i[
 #`---------'---------'---------'----------------------'----------------'---------'-------'-------'-------'
 
 kbd.add_layer :lower, %i[
-  KC_F1    KC_F2   KC_F3    KC_F4     KC_F5    KC_F6  KC_F7       KC_F8      KC_F9    KC_F10   KC_F11   KC_NO    KC_F12
-  KC_NO    KC_NO   KC_NO    KC_NO     KC_NO    KC_NO  KC_NO       KC_NO      KC_NO    KC_NO    KC_NO    KC_NO    KC_NO
-  KC_NO    KC_NO   KC_CAPS  KC_PAUSE  KC_NO    KC_NO  KC_NO       KC_NO      KC_NO    KC_NO    KC_NO    KC_NO    KC_KB_VOLUME_UP    KC_RSFT
-  KC_NO    KC_NO   KC_LCTL  KC_LGUI   KC_LALT  KC_NO  KC_TRNS     KC_PSCR    KC_NO    KC_NO    KC_RCTL  KC_NO    KC_KB_VOLUME_DOWN  KC_NO
+  KC_F1    KC_F2   KC_F3    KC_F4     KC_F5    KC_F6  KC_F7      KC_F8      KC_F9    KC_F10   KC_F11   KC_NO    KC_F12
+  KC_NO    KC_NO   KC_NO    KC_NO     KC_NO    KC_NO  KC_NO      KC_NO      KC_NO    KC_NO    KC_NO    KC_NO    KC_NO
+  KC_NO    KC_NO   KC_CAPS  KC_PAUSE  KC_NO    KC_NO  KC_NO      KC_NO      KC_NO    KC_NO    KC_NO    KC_NO    KC_KB_VOLUME_UP    KC_RSFT
+  KC_NO    KC_NO   KC_LCTL  KC_LGUI   KC_LALT  KC_NO  KC_TRNS    KC_PSCR    KC_NO    KC_NO    KC_RCTL  KC_NO    KC_KB_VOLUME_DOWN  KC_NO
 ]
 
 #,-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.
@@ -54,10 +54,10 @@ kbd.add_layer :lower, %i[
 #`---------'---------'---------'----------------------'----------------'---------'-------'-------'-------'
 
 kbd.add_layer :raise, %i[
-  KC_1     KC_2    KC_3      KC_4                KC_5     KC_6   KC_7        KC_8       KC_9     KC_0     KC_MINS       KC_NO          KC_EQL
-  KC_NO    KC_NO   KC_GRAVE  KC_NO               KC_NO    KC_NO  KC_NO       KC_NO      KC_LBRC  KC_RBRC  KC_SEMICOLON  KC_NONUS_HASH  KC_NO
-  KC_NO    KC_NO   KC_LSFT   KC_NONUS_BACKSLASH  KC_NO    KC_NO  KC_NO       KC_NO      KC_M     KC_COMM  KC_DOT        KC_SLSH        KC_KB_VOLUME_UP    KC_RSFT
-  KC_NO    KC_NO   KC_LCTL   KC_LGUI             KC_LALT  KC_NO  KC_KB_MUTE  KC_TRNS    KC_NO    KC_NO    KC_RALT       KC_NO          KC_KB_VOLUME_DOWN  KC_NO
+  KC_1     KC_2    KC_3      KC_4                KC_5     KC_6   KC_7        KC_8     KC_9     KC_0     KC_MINS       KC_NO          KC_EQL
+  KC_NO    KC_NO   KC_GRAVE  KC_NO               KC_NO    KC_NO  KC_NO       KC_NO    KC_LBRC  KC_RBRC  KC_SEMICOLON  KC_NONUS_HASH  KC_NO
+  KC_NO    KC_NO   KC_LSFT   KC_NONUS_BACKSLASH  KC_NO    KC_NO  KC_NO       KC_NO    KC_M     KC_COMM  KC_DOT        KC_SLSH        KC_KB_VOLUME_UP    KC_RSFT
+  KC_NO    KC_NO   KC_LCTL   KC_LGUI             KC_LALT  KC_NO  KC_KB_MUTE  KC_TRNS  KC_NO    KC_NO    KC_RALT       KC_NO          KC_KB_VOLUME_DOWN  KC_NO
 ]
 
 #
